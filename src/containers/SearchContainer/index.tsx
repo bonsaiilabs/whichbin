d518f0cba850682a803083595b14a5e52500eb54
hhimanshu <harit.himanshu@yahoo.in>
2021-07-27 11:44:23 -0700

    feat(DEV-38): got algolia search working based on their starter code

---
+++ src/containers/SearchContainer/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import algoliasearch from 'algoliasearch';
+import { Autocomplete } from '../../components/Autocomplete';
+import './search.css'
+import '@algolia/autocomplete-theme-classic';
+
+
+const appId = 'latency';
+const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
+const searchClient = algoliasearch(appId, apiKey);
+
+function SearchContainer() {
+  return (
+    <div className="container">
+      <Autocomplete placeholder="Search" openOnFocus={true} debug={true} />
+    </div>
+  );
+}
+export default SearchContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,14 @@ import '@fontsource/rubik/700.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomeContainer } from './containers/HomeContainer';
 import { About } from './components/Starter/About';
+import SearchContainer from './containers/SearchContainer';
 
 // This is where your application content can be mounted
 const Root = () => (
   <Router>
     <Switch>
-      <Route path='/' exact component={HomeContainer} />
+      {/*<Route path='/' exact component={HomeContainer} />*/}
+      <Route path='/' exact component={SearchContainer} />
       <Route path='/about' component={About} />
     </Switch>
   </Router>
