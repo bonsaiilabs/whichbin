9cff6382287212c5310a65142409be5916bbadfa
hhimanshu <harit.himanshu@yahoo.in>
2021-07-28 12:39:26 -0700

    feat(DEV-38): convert list to map for efficient search

--- src/containers/SearchContainer/index.tsx
+++ src/containers/SearchContainer/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Autocomplete } from '../../components/Autocomplete';
 import styles from './search.module.css';
 import '@algolia/autocomplete-theme-classic';
+import { itemToBins } from '../../data/bins';
+import { ItemToBin } from '../../types/itemToBin';
 
 function SearchContainer() {
+  const [idsToBin, setIdsToBin] = useState<Map<number, ItemToBin> | undefined>(undefined)
+
+  useEffect(() => {
+    const itemIdsToBin = itemToBins.reduce(((acc: Map<number, ItemToBin>, item) => {
+      acc.set(item.id, item);
+      return acc;
+    }), new Map<number, ItemToBin>());
+    console.log(itemIdsToBin)
+    setIdsToBin(itemIdsToBin)
+  }, []);
+
   return (
     <div className={styles.container}>
       <Autocomplete placeholder='Search' openOnFocus={true} debug={true} />
     </div>
   );
 }
+
 export default SearchContainer;
